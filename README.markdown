3cbccf323b42fbd87aed4eafe53168c7f71fe9d6
kyo_ago <kyo@0-9.sakura.ne.jp>
2010-05-18 20:16:29 +0900

    first commit

---
+++ README.markdown
@@ -0,0 +1,29 @@
+カーソルキーでエントリーを移動するjQuery plugin
+
+#### 使い方
+
+	$(function () {
+		$.arrowkey_scroll({
+			// スクロールのエフェクト名 
+			// 初期値「quart」 
+			// quartは設定されていない場合、内部のものを設定する 
+			'scroll_type' : 'quart',
+			// スクロール時間 
+			// 初期値900 
+			'time' : 900,
+			// 各entryのCSS Selector 
+			'selector' : '.entryWrapper'
+		});
+	})
+
+#### 読み込み済み画像
+
+使用される際は是非以下のマークも表示させてみてください。  
+（必須ではありません）
+
+![keybord mark](http://tech.kayac.com/img2/img_keybord_01.png)  
+（画像URLが変更になる可能性があるので、画像はコピーしてご使用ください）
+
+#### 紹介記事
+
+[カーソルキーでエントリーを移動するjQuery pluginを作成しました | tech.kayac.com - KAYAC engineers' blog](http://tech.kayac.com/archive/jquery-arrowkey_scroll-plugin.html)
---
+++ jquery.keyscroll.js
@@ -0,0 +1,60 @@
+/*!
+ * jquery arrowkey_scroll plugin
+ * Copyright (C) KAYAC Inc. | http://www.kayac.com/
+ * Dual licensed under the MIT <http://www.opensource.org/licenses/mit-license.php>
+ * and GPL <http://www.opensource.org/licenses/gpl-license.php> licenses.
+ * Date: 2010-05-18
+ * @author kyo_ago
+ * @version 1.0.0
+ * @require jQuery 1.3 or later
+ * @see http://github.com/kyo-ago/jQuery.arrowkey_scroll
+ */
+$.arrowkey_scroll = function (option) {
+	option = $.extend({
+		'scroll_type' : 'quart',
+		'time' : 900,
+		'selector' : '.entryWrapper'
+	}, option);
+	//smooth scroll
+	$.easing.quart = $.easing.quart || function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	};
+	$.fn.smooth_scroll = $.fn.smooth_scroll || function () {
+		$('html, body').stop().animate({
+			'scrollTop' : this.offset().top
+		}, option.time, option.scroll_type);
+	};
+	$(function () {
+		$($.browser.msie ? 'body' : window).keyup(function (e) {
+			if (!(e.keyCode == 37 || e.keyCode == 39)) return;
+			if ($(e.source).is(':input')) return;
+			e.preventDefault();
+			$('html, body').stop();
+			var top = +(
+				window.pageYOffset
+				|| document.documentElement.scrollTop
+				|| document.body.scrollTop
+			) || 0;
+			var last_scroll = setTimeout(function () {
+				if (!$('html, body').is(':animated')) $('.pager:last').smooth_scroll();
+			}, 0);
+			var entrys = $(option.selector);
+			entrys.each(function (index) {
+				var offset = ~~($(this).offset().top) || 0;
+				// <- key
+				if (e.keyCode == 37) {
+					var len = entrys.last().filter(this).length;
+					if (!len && (offset < top)) return;
+					if (len && (offset < top)) return $(this).smooth_scroll();
+					$(this).add(entrys.eq(index-1)).eq(0).smooth_scroll();
+					return false;
+				};
+				// -> key
+				if (offset <= top) return;
+				$(this).smooth_scroll();
+				clearTimeout(last_scroll);
+				return false;
+			});
+		});
+	});
+};
